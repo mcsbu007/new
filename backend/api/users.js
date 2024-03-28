0983a8e752fc9d1c3fe9b7194a32c341737f0123
yunjia832 <112725862+523243240@users.noreply.github.com>
2024-03-28 15:24:56 -0400

    Update users.js

--- backend/api/users.js
+++ backend/api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/User');
 
-router.get("/", (req, res) => {
+router.get("/api/users", (req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => console.log(err))
