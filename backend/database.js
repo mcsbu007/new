e98246ad3e4e995cb9fda67360ce0b695692ca82
yunjia832 <112725862+523243240@users.noreply.github.com>
2024-03-28 14:38:35 -0400

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://yunjiani832:TmeCv3DruDyhXCdg@cluster0.yh3beol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const connection = "mongodb+srv://yunjiani832:RPehVJ1wqYUdc0PS@cluster0.yh3beol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
